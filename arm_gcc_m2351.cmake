09a23a88156332c4c393f55e79c137e08bad16a0
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2022-11-11 13:21:41 +0000

    build system modifications

--- arm_gcc_m2351.cmake
+++ arm_gcc_m2351.cmake
@@ -5,6 +5,8 @@ set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
 
+set(CMAKE_ASM_COMPILER_TARGET "${CMAKE_CXX_COMPILER_TARGET}")
+
 set(COMMON_FLAGS "-mcpu=cortex-m23 -DCORTEX_M23 -march=armv8-m.base -mthumb -mlittle-endian -ffunction-sections -fdata-sections -finline-functions -funsigned-char -Os -g -Wl,--gc-sections")
 
 set(CMAKE_CXX_FLAGS "--specs=nano.specs --specs=nosys.specs ${COMMON_FLAGS}")
